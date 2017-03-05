ffa2dbe90491aa25e2d2e427e89b5c00751af96a
Santina Lin <santina424@gmail.com>
2017-03-05 08:22:33 -0800

    Move buttons to left margin in search result

--- content.js
+++ content.js
@@ -3,31 +3,38 @@ var btn = document.createElement("button");
   var text = document.createTextNode("Verify");
   btn.appendChild(text);
 
+
 chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
 	switch(message.type) {
 		case "colors-div":
-			var divs = document.querySelectorAll(".r");
+			var divs = document.querySelectorAll(".g");
 			if(divs.length === 0) {
 				alert("There are no any divs in the page.");
 			} else {
 				for(var i=0; i<divs.length; i++) {
 					divs[i].style.backgroundColor = message.color;
-					var newEl = document.createElement('p');
-					newEl.appendChild(document.createElement('btn')).textContent="VERIFY";
-					newEl.style.border = "2px solid #000000";
-					newEl.style.backgroundColor = "#FF4500";
+					var newEl = document.createElement('btn');
+
+					newEl.textContent="check quality";
+					newEl.style.backgroundColor = "#008CBA";
 					newEl.style.textAlign = "center";
 					newEl.style.color = "#FFFFFF";
 					newEl.addEventListener("click", function(){
 						alert("I'm clicked!");
 					}, false);
 					divs[i].insertBefore(newEl, divs[i].childNodes[0]);
+
 				}
+        var buttons = document.querySelectorAll("btn");
+        for(var i=0; i<buttons.length; i++) {
+          buttons[i].style.position="absolute";
+          buttons[i].style.left="-110px";
+        }
 
 			}
 		break;
 
 
 	}
-  
+
 });
